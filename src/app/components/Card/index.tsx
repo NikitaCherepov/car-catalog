4189d95ef829b49df367d65c8e6eaa9116f1eb03
NikitaCherepov <100406501+NikitaCherepov@users.noreply.github.com>
2025-06-15 07:52:54 +0700

    [FIX] - Fixed optimization for images

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -26,15 +26,14 @@ export default function Card({name, image, price, modification, run, year}:CardP
         className={styles.container}>
             <Image 
             className={styles.container__image} 
-            width={100} 
-            height={100}
+            width={285} 
+            height={215}
             src={image} 
             quality={100}
             alt='Image car'
             priority 
             placeholder="blur"
-            blurDataURL="/icons/waiting.svg"
-            unoptimized/>
+            blurDataURL="/icons/waiting.svg"/>
             <div className={styles.container__info}>
                 <p className={styles.container__info__header}>
                     <strong>
